f813d840d7ad982e9f8371b8c129193c465bc596
muukii <muukii.muukii@gmail.com>
2015-07-28 01:09:08 +0900

    Initial Commit

---
+++ LightRoom/LightRoom.h
@@ -0,0 +1,19 @@
+//
+//  LightRoom.h
+//  LightRoom
+//
+//  Created by Muukii on 7/28/15.
+//  Copyright © 2015 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightRoom.
+FOUNDATION_EXPORT double LightRoomVersionNumber;
+
+//! Project version string for LightRoom.
+FOUNDATION_EXPORT const unsigned char LightRoomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightRoom/PublicHeader.h>
+
+
