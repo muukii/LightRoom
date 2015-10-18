13d02b48f90fd25441779bfcb8bac7fec4002858
muukii <muukii.muukii@gmail.com>
2015-10-18 17:07:57 +0900

    wip :D

--- Playgrounds/Main.playground/Pages/test.xcplaygroundpage/Contents.swift
+++ Playgrounds/Main.playground/Pages/test.xcplaygroundpage/Contents.swift
@@ -6,15 +6,16 @@ import SwiftyJSON
 
 let image = CIImage(data: NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("sample1", ofType: "jpg")!)!)!
 
-LightRoom.Stylize.HighlightShadowAdjust(highlightAmount: 1, shadowAmount: -0.5).filter(image)
+LightRoom.Stylize.HighlightShadowAdjust(highlightAmount: 2, shadowAmount: 0).filter(image)
 LightRoom.ColorAdjustment.GammmaAdjust(power: 0.7).filter(image)
 
-let amount: Double = 0.8
-(
-    LightRoom.ColorAdjustment.Vibrance(amount: amount * 0.2).filter >>>
-        LightRoom.Stylize.HighlightShadowAdjust(highlightAmount: 1, shadowAmount: amount * 0.2).filter >>>
-        LightRoom.Sharpen.UnsharpMask(radius: 1.5, intencity: amount * 1.4).filter
-)(image)
+
+//let amount: Double = 0.8
+//(
+//    LightRoom.ColorAdjustment.Vibrance(amount: amount * 0.2).filter >>>
+//        LightRoom.Stylize.HighlightShadowAdjust(highlightAmount: 1, shadowAmount: amount * 0.2).filter >>>
+//        LightRoom.Sharpen.UnsharpMask(radius: 1.5, intencity: amount * 1.4).filter
+//)(image)
 
 
 
