28eaaede59843f290d75b70a4344f2db1b9dd362
muukii <muukii.muukii@gmail.com>
2015-10-20 19:22:57 +0900

    Add GeometryAdjustment

---
+++ LightRoom/Classes/GeometryAdjustment.swift
@@ -0,0 +1,31 @@
+//
+//  GeometryAdjustment.swift
+//  LightRoom
+//
+//  Created by Muukii on 10/20/15.
+//  Copyright © 2015 muukii. All rights reserved.
+//
+
+import Foundation
+
+public extension LightRoom {
+    
+    /**
+    CILanczosScaleTransform
+    Produces a high-quality, scaled version of a source image.
+    */
+    public static func lanczosScaleTransform(scale scale: Double, aspectRatio: Double) -> Filter {
+        
+        return { image in
+            
+            let parameters = [
+                kCIInputImageKey: image,
+                "inputScale": scale,
+                "inputAspectRatio" : aspectRatio,
+            ]
+            
+            let filter = CIFilter(name: "CILanczosScaleTransform", withInputParameters: parameters)
+            return filter!.outputImage!
+        }
+    }
+}
