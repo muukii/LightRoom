4f0b0bdb933a2716689a027a2b87d436289e6fa4
muukii <m@muukii.me>
2016-09-30 00:34:51 +0900

    Add @discardableResult

--- LightRoom/Classes/Operators.swift
+++ LightRoom/Classes/Operators.swift
@@ -18,42 +18,50 @@ Set inputBackgroundImage on CompositionComponentType
 */
 infix operator --* : MultiplicationPrecedence
 
+@discardableResult
 public func >>> (chain1: FilterChain, chain2: FilterChain) -> FilterChain {
     chain2.inputImage = chain1.outputImage
     _ = chain1.connect(chain2)
     return chain2
 }
 
+@discardableResult
 public func >>> (image: CIImage?, chain: FilterChain) -> FilterChain {
     chain.inputImage = image
     return chain
 }
 
+@discardableResult
 public func >>> (image: CIImage?, component: FilterComponentType) -> FilterComponentType {
     component.inputImage = image
     return component
 }
 
+@discardableResult
 public func >>> (chain1: FilterComponentType, component: FilterComponentType) -> FilterComponentType {
     component.inputImage = chain1.outputImage
     return component
 }
 
+@discardableResult
 public func >>> (gen: GeneratorComponent, component: FilterComponentType) -> FilterComponentType {
     component.inputImage = gen.outputImage
     return component
 }
 
+@discardableResult
 public func --* (chain1: FilterComponentType, component: CompositionFilterComponent) -> FilterComponentType {
     component.inputBackgroundImage = chain1.outputImage
     return component
 }
 
+@discardableResult
 public func --* (image: CIImage?, component: CompositionFilterComponent) -> FilterComponentType {
     component.inputBackgroundImage = image
     return component
 }
 
+@discardableResult
 public func --* (gen: GeneratorComponent, component: CompositionFilterComponent) -> FilterComponentType {
     component.inputBackgroundImage = gen.outputImage
     return component
